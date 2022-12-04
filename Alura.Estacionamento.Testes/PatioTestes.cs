29ad56df899a4d1c16017fbe1e9fd775e375ec96
Walter <silvawalteralipio@gmail.com>
2022-12-04 14:43:45 -0300

    add new tests

--- Alura.Estacionamento.Testes/PatioTestes.cs
+++ Alura.Estacionamento.Testes/PatioTestes.cs
@@ -60,4 +60,62 @@ public class PatioTestes
     //Assert
     Assert.Equal(2, invoicing);
   }
+
+  [Theory]
+  [InlineData("André Silva", "ASD-1498", "Preto", "Gol")]
+  public void TestFindVehicleInParking
+  (
+    string owner,
+    string licensePlate,
+    string color,
+    string model
+  )
+  {
+    //Arrange
+    var testOperator = new Operador { Nome = "Michel" };
+    var parking = new Patio(testOperator);
+    var vehicle = new Veiculo();
+
+    vehicle.Proprietario = owner;
+    vehicle.Placa = licensePlate;
+    vehicle.Cor = color;
+    vehicle.Modelo = model;
+
+    parking.RegistrarEntradaVeiculo(vehicle);
+
+    //Act
+    var searched = parking.PesquisaVeiculoPorPlaca(licensePlate);
+    //Assert
+    Assert.Equal(licensePlate, searched.Placa);
+  }
+
+  [Fact]
+  public void TestChangeVehicleData()
+  {
+    // Given
+    var testOperator = new Operador { Nome = "Michel" };
+    var parking = new Patio(testOperator);
+
+    var vehicle = new Veiculo();
+    vehicle.Proprietario = "Walter Alípio";
+    vehicle.Tipo = TipoVeiculo.Automovel;
+    vehicle.Cor = "Azul";
+    vehicle.Modelo = "Astra";
+    vehicle.Placa = "ads-9992";
+
+    parking.RegistrarEntradaVeiculo(vehicle);
+
+    var changedVehicle = new Veiculo();
+    changedVehicle.Proprietario = "Walter Alípio";
+    changedVehicle.Tipo = TipoVeiculo.Automovel;
+    changedVehicle.Cor = "preto";
+    changedVehicle.Modelo = "Opala";
+    changedVehicle.Placa = "ads-9992";
+
+
+    // When
+    var changed = parking.AlteraDadosVeiculo(changedVehicle);
+    // Then
+    Assert.Equal(changed.Cor, changedVehicle.Cor);
+  }
 }
--- Alura.Estacionamento.Testes/VeiculoTestes.cs
+++ Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -6,8 +6,9 @@ namespace Alura.Estacionamento.Testes;
 
 public class VeiculoTestes
 {
-  [Fact]
-  public void TestVehicleAcelerate()
+  [Fact(DisplayName = "Teste 1")]
+  [Trait("Function", "Speed Up")]
+  public void TestVehicleSpeedUp()
   {
     //Arrange
     var vehicle = new Veiculo();
@@ -17,7 +18,8 @@ public class VeiculoTestes
     Assert.Equal(100, vehicle.VelocidadeAtual);
   }
 
-  [Fact]
+  [Fact(DisplayName = "Teste 2")]
+  [Trait("Function", "Break")]
   public void TestVehicleBreak()
   {
     //Arrange
@@ -39,4 +41,43 @@ public class VeiculoTestes
     Assert.Equal(TipoVeiculo.Automovel, vehicle.Tipo);
   }
 
+  [Fact(Skip = "Não implementado")]
+  public void TestOwnerName()
+  {
+    // Arrange
+
+    // Act
+
+    // Assert
+  }
+
+  [Theory]
+  [ClassData(typeof(Veiculo))]
+  public void TestVehicleClass(Veiculo model)
+  {
+    //Arrange
+    var vehicle = new Veiculo();
+    //Act
+    vehicle.Acelerar(10);
+    model.Acelerar(10);
+    //Assert
+    Assert.Equal(model.VelocidadeAtual, vehicle.VelocidadeAtual);
+  }
+
+  [Fact]
+  public void TestVehicleData()
+  {
+    // Arrange
+
+    var vehicle = new Veiculo();
+    vehicle.Proprietario = "Walter Alípio";
+    vehicle.Tipo = TipoVeiculo.Automovel;
+    vehicle.Cor = "Azul";
+    vehicle.Modelo = "Astra";
+    vehicle.Placa = "ads-9992";
+    // Act
+    string data = vehicle.ToString();
+    // Assert
+    Assert.Contains("Tipo do Veículo: Automovel", data);
+  }
 }
