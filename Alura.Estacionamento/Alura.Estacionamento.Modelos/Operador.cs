7bb55ffad42fe5ba8e22c042f32ee20a7091c33c
Walter <silvawalteralipio@gmail.com>
2022-12-05 15:05:42 -0300

    adjusted on Operador

--- Alura.Estacionamento/Alura.Estacionamento.Modelos/Operador.cs
+++ Alura.Estacionamento/Alura.Estacionamento.Modelos/Operador.cs
@@ -6,21 +6,28 @@ using System.Threading.Tasks;
 
 namespace Alura.Estacionamento.Modelos
 {
-    public class Operador
+  public class Operador
+  {
+    // Campos
+    private string _matricula;
+    private string _nome;
+
+    //Propriedades
+    public string Matricula { get => _matricula; private set => _matricula = value; }
+    public string Nome { get => _nome; set => _nome = value; }
+
+    //Construtor
+    public Operador()
     {
-        // Campos
-        private string _matricula;
-        private string _nome;
-
-        //Propriedades
-        public string Matricula { get => _matricula;  private set => _matricula = value; }
-        public string Nome { get => _nome; set => _nome = value; }
-
-        //Construtor
-        public Operador()
-        {
-            this.Matricula = new Guid().ToString().Substring(0,8);
-        }
-
+      this.Matricula = new Guid().ToString().Substring(0, 8);
     }
+
+    public override string ToString()
+    {
+      return $@"
+            Operador: {this.Nome}
+            Matricula: {this.Matricula}";
+    }
+
+  }
 }
